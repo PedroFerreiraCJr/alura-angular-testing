7f9cb140341b3a736e209b06b6b1ff0bf1f32e1d
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-08-08 20:25:41 -0300

    refatoração para aprimorar o método de geração de uuid; usando withContext para esclarecer o contexto de execução

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -20,15 +20,27 @@ describe('O artefato que queremos testar', () => {
 */
 const APP_PREFIX = 'app';
 
+
 describe(UniqueIdService.name, () => {
-    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
-        const service  = new UniqueIdService();
+    let service: UniqueIdService = null;
+    
+    /*
+     Antes de cada teste declarado com a função it, este bloco de código será executado, obtendo assim, para cada teste\
+     uma nova instância de UniqueIdService.
+     Cada teste criado deve ser independente, não mantendo estado entre as execuções de cada it - caso de teste.
+    */
+    beforeEach(() => {
+        service = new UniqueIdService();
+    });
+
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+     should generate id when called with prefix`, () => {
         const id = service.generateUniqueIdWithPrefix(APP_PREFIX);
         expect(id.startsWith(`${APP_PREFIX}-`)).toBeTrue();
     });
 
-    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
-        const service = new UniqueIdService();
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+     should not generate duplicate IDs when called multiple times`, () => {
         const uuids = new Set();
         for (let i=0; i<50; i++) {
             uuids.add(service.generateUniqueIdWithPrefix(APP_PREFIX));
@@ -36,9 +48,22 @@ describe(UniqueIdService.name, () => {
         expect(uuids.size).toEqual(50);
     });
 
-    it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generated IDs when called`, () => {
-        const service = new UniqueIdService;
+    it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+     should return the number of generated IDs when called`, () => {
+        service.generateUniqueIdWithPrefix(APP_PREFIX);
         service.generateUniqueIdWithPrefix(APP_PREFIX);
         expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
     });
+
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+     should throw when called with empty`, () => {
+         const emptyValues = [null, undefined, ''];
+         emptyValues.forEach((value) => {
+            expect(() => {
+                service.generateUniqueIdWithPrefix(value);
+             })
+             // o withContext deve ser utilizado quando estiver utilizando um loop para verificar várias expectativas
+             .withContext(`Empty value - ${value}`).toThrow();
+         });
+     });
 });
